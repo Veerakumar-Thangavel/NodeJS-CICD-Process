7a63c879d3805a9e647f06d9e8e1205597aab946
Veerakumar-Thangavel <130571444+Veerakumar-Thangavel@users.noreply.github.com>
2025-07-19 09:18:51 +0530

    Update outout.tf

--- Infrastructure/outout.tf
+++ Infrastructure/outout.tf
@@ -1,45 +1,218 @@
-# Outputs
-#output "vpc_id" {
-  #description = "ID of the VPC"
-  #value       = module.network.vpc_id
-# }
-
-output "bastion_public_ip" {
-  description = "Public IP of the bastion host"
-  value       = aws_instance.bastion_host.public_ip
+# -------------------- 0. Key Pair (PEM) --------------------
+resource "tls_private_key" "ec2_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
 }
 
-output "application_private_ip" {
-  description = "Private IP of the application server"
-  value       = aws_instance.application.private_ip
+resource "aws_key_pair" "nodejs_key" {
+  key_name   = "nodejs-key"
+  public_key = tls_private_key.ec2_key.public_key_openssh
 }
 
-output "ssm_parameter_name" {
-  description = "SSM parameter storing private key"
-  value       = aws_ssm_parameter.private_key.name
+resource "local_file" "private_key" {
+  content              = tls_private_key.ec2_key.private_key_pem
+  filename             = "${path.module}/nodejs-key.pem"
+  file_permission      = "0400"
+  directory_permission = "0700"
 }
 
-output "ssh_key_instructions" {
-  value = "aws ssm get-parameter --name ${aws_ssm_parameter.private_key.name} --with-decryption --query Parameter.Value --output text > private_key.pem && chmod 400 private_key.pem"
+# -------------------- 1. VPC --------------------
+resource "aws_vpc" "node_js_vpc" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Name = var.project_tag
+  }
 }
 
+# -------------------- 2. Public Subnet --------------------
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.node_js_vpc.id
+  cidr_block              = var.public_subnet_cidr
+  map_public_ip_on_launch = true
+  availability_zone       = "${var.region}a"
+
+  tags = {
+    Name = "${var.project_tag}-public"
+  }
+}
+
+# -------------------- 3. Private Subnet --------------------
+resource "aws_subnet" "private_subnet" {
+  vpc_id            = aws_vpc.node_js_vpc.id
+  cidr_block        = var.private_subnet_cidr
+  availability_zone = "${var.region}b"
+
+  tags = {
+    Name = "${var.project_tag}-private"
+  }
+}
+
+# -------------------- 4. Internet Gateway --------------------
+resource "aws_internet_gateway" "my_internet_gateway" {
+  vpc_id = aws_vpc.node_js_vpc.id
+
+  tags = {
+    Name = var.project_tag
+  }
+}
+
+# -------------------- 5. Route Tables --------------------
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.node_js_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.my_internet_gateway.id
+  }
+
+  tags = {
+    Name = "${var.project_tag}-public"
+  }
+}
+
+resource "aws_route_table_association" "public_route_table_association" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+resource "aws_eip" "nat_eip" {
+  domain = "vpc"
+}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = aws_subnet.public_subnet.id
+  depends_on    = [aws_internet_gateway.my_internet_gateway]
+}
+
+resource "aws_route_table" "private_route_table" {
+  vpc_id = aws_vpc.node_js_vpc.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat.id
+  }
+
+  tags = {
+    Name = "${var.project_tag}-private"
+  }
+}
+
+resource "aws_route_table_association" "private_route_table_association" {
+  subnet_id      = aws_subnet.private_subnet.id
+  route_table_id = aws_route_table.private_route_table.id
+}
+
+# -------------------- 6. Security Groups --------------------
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow-ssh"
+  vpc_id      = aws_vpc.node_js_vpc.id
+  description = "Allow SSH from anywhere"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "allow_ssh_and_port_3000" {
+  name        = "allow-ssh-3000"
+  description = "Allow SSH and App Port 3000"
+  vpc_id      = aws_vpc.node_js_vpc.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "allow_jenkins" {
+  name        = "allow-jenkins"
+  description = "Allow Jenkins UI"
+  vpc_id      = aws_vpc.node_js_vpc.id
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# -------------------- 7. Bastion Host with EIP --------------------
+resource "aws_instance" "bastion_host" {
+  ami                         = var.ubuntu_ami_id
+  instance_type               = var.instance_type
+  subnet_id                   = aws_subnet.public_subnet.id
+  vpc_security_group_ids      = [aws_security_group.allow_ssh.id, aws_security_group.allow_jenkins.id]
+  key_name                    = aws_key_pair.nodejs_key.key_name
+  associate_public_ip_address = true
+  user_data                   = file("${path.module}/shell.sh")
+
+  tags = {
+    Name = "${var.project_tag}-bastion"
+  }
+
+  depends_on = [aws_internet_gateway.my_internet_gateway]
+}
+
+resource "aws_eip" "bastion_eip" {
+  instance = aws_instance.bastion_host.id
+  vpc      = true
+}
+
+# -------------------- 8. Application Host --------------------
+resource "aws_instance" "application" {
+  ami                    = var.ubuntu_ami_id
+  instance_type          = var.instance_type
+  subnet_id              = aws_subnet.private_subnet.id
+  vpc_security_group_ids = [aws_security_group.allow_ssh_and_port_3000.id]
+  key_name               = aws_key_pair.nodejs_key.key_name
+
+  tags = {
+    Name = "${var.project_tag}-app"
+  }
+}
+
+# -------------------- 9. Store Private Key in SSM --------------------
 resource "aws_ssm_parameter" "private_key" {
-  name  = "/ec2/keypair/private"
-  type  = "SecureString"
-  value = file("${path.module}/id_rsa") # Make sure id_rsa is present
-  overwrite   = true             
+  name        = "/ec2/keypair/private"
+  type        = "SecureString"
+  value       = tls_private_key.ec2_key.private_key_pem
+  overwrite   = true
 }
-
-
-#output "vpc_id" {
- # value = aws_vpc.node_js_vpc.id
-#}
-
-output "public_subnet_id" {
-  value = aws_subnet.public_subnet.id
-}
-
-output "private_subnet_id" {
-  value = aws_subnet.private_subnet.id
-}
-
